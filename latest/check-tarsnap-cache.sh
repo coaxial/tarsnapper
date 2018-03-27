408f6979f46686c2d43c9b8f0b5d5823dfbc2614
coaxial <hi@64b.it>
2018-03-27 09:39:27 -0400

    Remove latest

--- latest/check-tarsnap-cache.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Only rebuilds the cache if the directory is empty. --fsck costs money because
-# it's transferring data.
-if [ -z "$(ls -A /usr/local/tarsnap-cache/)" ]; then
-  tarsnap --fsck
-fi
--- latest/run-backup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-/check-tarsnap-cache.sh &&\
-tarsnapper -c /etc/tarsnapper.conf make
